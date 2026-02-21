8af08d4c38b974b182cda20981306a510141f9ea
1920xl080 <arrigo1888@gmail.com>
2026-02-21 20:07:27 +0700

    feat: added Map and Mapb functions with PokeAPI integration

--- repl.go
+++ repl.go
@@ -1,72 +1,182 @@
 package main
 
-import ("strings"
-		"os"
-		"bufio"
-		"fmt")
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+var client = &http.Client{
+	Timeout: 30 * time.Second,
+}
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
-
+	callback    func(*config) error
 }
 
-func getCliCommands()map[string]cliCommand{
-	var cliCommands = map[string]cliCommand{
+type config struct {
+	Next     *string
+	Previous *string
+}
+
+type locationResponse struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func getCliCommands() map[string]cliCommand {
+	return map[string]cliCommand{
 		"exit": {
-			name:        "exit", 
+			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
 		"help": {
-			name:        "help", 
+			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays locations of the pokemon world",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 locations",
+			callback:    commandMapb,
+		},
 	}
-	return cliCommands
 }
 
-
-
-func cleanInput(text string)[]string{
-	str_array := strings.Fields(text)
-	for idx,val := range str_array{
-		str_array[idx]=strings.ToLower(val)
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+	for i, val := range words {
+		words[i] = strings.ToLower(val)
 	}
-	return str_array
+	return words
 }
 
-func startRepl(){
+func startRepl(cfg *config) {
 	cliCommands := getCliCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		if has_value := scanner.Scan();has_value{
+		if ok := scanner.Scan(); ok {
 			cleanCommand := cleanInput(scanner.Text())
-			if val,bool:=cliCommands[cleanCommand[0]]; bool==true{
-				val.callback()
-			}else{
-				fmt.Print("Unknown command")
+			if len(cleanCommand) == 0 {
+				continue
+			}
+			if command, exists := cliCommands[cleanCommand[0]]; exists {
+				err := command.callback(cfg)
+				if err != nil {
+					fmt.Printf("%v\n", err)
+				}
+			} else {
+				fmt.Println("Unknown command")
 			}
 		}
-		fmt.Print("\n")
 	}
 }
 
-func commandExit() error{
-	fmt.Print("Closing the Pokedex... Goodbye!")
+func commandExit(*config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error{
+func commandHelp(*config) error {
 	cliCommands := getCliCommands()
-	fmt.Print("Welcome to the Pokedex!\n")
+	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
-	for _,value := range cliCommands{
-		fmt.Printf("\n%s: %s",value.name,value.description)
+
+	keys := make([]string, 0, len(cliCommands))
+	for k := range cliCommands {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		command := cliCommands[k]
+		fmt.Printf("\n%s: %s", command.name, command.description)
+	}
+	fmt.Println("\n")
+	return nil
+}
+
+func fetchLocations(url string) (locationResponse, error) {
+	var response locationResponse
+	if url == "" {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return response, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return response, err
+	}
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&response); err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.Previous == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	response, err := fetchLocations(*cfg.Previous)
+	if err != nil {
+		return err
+	}
+
+	cfg.Next = response.Next
+	cfg.Previous = response.Previous
+
+	for _, val := range response.Results {
+		fmt.Printf("%s\n", val.Name)
+	}
+	return nil
+}
+
+func commandMap(cfg *config) error {
+	url := ""
+	if cfg.Next != nil {
+		url = *cfg.Next
+	}
+
+	response, err := fetchLocations(url)
+	if err != nil {
+		return err
+	}
+
+	cfg.Next = response.Next
+	cfg.Previous = response.Previous
+
+	for _, val := range response.Results {
+		fmt.Printf("%s\n", val.Name)
 	}
 	return nil
 }
